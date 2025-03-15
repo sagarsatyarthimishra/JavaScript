98ecaa3c4281c158cb9c916d909cd0acb930d78e
Sagar Satyarthi Mishra <140907393+sagarsatyarthimishra@users.noreply.github.com>
2025-03-15 06:59:26 +0000

    commit

--- basic/03_conversionOperation.js
+++ basic/03_conversionOperation.js
@@ -1,26 +1,37 @@
-let score = undefined;
+// let score = undefined;
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score); // type conversion string to number
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score); // type conversion string to number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
+// // "33" => 33
+// // "33ass" => NaN
+// // true => 1; false => 0
 
-// "33" => 33
-// "33ass" => NaN
-// true => 1; false => 0
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn= Boolean(isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.table([isLoggedIn,booleanIsLoggedIn,typeof(booleanIsLoggedIn)])
 
-let isLoggedIn = 1;
-let booleanIsLoggedIn= Boolean(isLoggedIn);
-console.log(typeof booleanIsLoggedIn);
-console.table([isLoggedIn,booleanIsLoggedIn,typeof(booleanIsLoggedIn)])
+// // 1 => true; 0 => false
+// //"" => false
 
-// 1 => true; 0 => false
-//"" => false
+// let someNumber = 33;
+// let stringSomeNumber = String(someNumber);
+// console.table([,someNumber,stringSomeNumber,typeof(stringSomeNumber)]);
 
-let someNumber = 33;
-let stringSomeNumber = String(someNumber);
-console.table([,someNumber,stringSomeNumber,typeof(stringSomeNumber)]);
+//*****************Operation*****************************/
 
+let value = 3;
+let negValue = -value;
+
+let str1 = "hello";
+let str2 = " sagar";
+let str3 = str1 + str2;
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+console.table([value, negValue, str3, +true, +"",num1]);
